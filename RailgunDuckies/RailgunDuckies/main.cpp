7c24b3cbae1e54ed06f6c69b0d26a07655337d48
Craig Day <craigday3@gmail.com>
2012-09-20 21:17:05 -0500

    added ducky and railgun class

---
+++ RailgunDuckies/RailgunDuckies/main.cpp
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////////////////////////
+/////*********************************************************************/////
+//   CS 559                                                                  //
+//   Project 1: Railgun Duckies                                              //
+//                                                                           //
+//   Authors:                                                                //
+//    Craig Day                                                              //
+//    Adam Hart                                                              //
+//                                                                           //
+//   University of Wisconsin-Madison                                         //
+/////*********************************************************************/////
+///////////////////////////////////////////////////////////////////////////////
+
+
+
+//Include any necessary libraries
+
+//Define any global variables
+
+//Set up world and viewport
+
+//* MAIN METHOD   //////////////////////////
+//
+//  -Initialize the skybox
+//  -place the railgun in the world
+//  -place any lights in the world
+//  -create/shoot ducks as needed
+//  -create/move baloons as needed
+//  -keep track of score
+//////////////////////////////////////////*/
+
+
---
+++ RailgunDuckies/RailgunDuckies/railGun.cpp
@@ -0,0 +1,20 @@
+
+
+class railGun {
+
+	private:
+		railGun(double pivotX, double pivotY, double pivotZ);
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
